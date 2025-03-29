8253956fb197ff004c67bd7acc24250f882f2bfd
Bailey Townsend <baileytownsend2323@gmail.com>
2025-03-29 13:47:36 -0500

    Fixed a bug and updated citro2d-rs to the cleaned version

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,6 @@ fn main() {
                 right_color: clr_tri2,
                 depth: 0.0,
             });
-            render_target.clear(clr_blue);
 
             render_target.render_2d_shape(&Rectangle {
                 point: Point::new(350.0, 0.0, 0.0),
